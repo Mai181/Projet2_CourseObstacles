c5be075fd613465b9d35d7534e6aeda675c26d04
ProfDave <dave.lacourse@cegeptr.qc.ca>
2023-02-17 10:12:22 -0500

    Joueur + mouvements

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Attributs
+    [SerializeField] private float _vitesse = 10f;
+    
+    //  Méthodes privées
+    private void Start()
+    {
+        // Position initiale du joueur
+        transform.position = new Vector3(-12f, 0.51f, -12f);
+    }
+
+    private void Update()
+    {
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float positionX = Input.GetAxis("Horizontal");
+        float positionZ = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(positionX, 0f, positionZ);
+        transform.Translate(direction * Time.deltaTime * _vitesse);
+    }
+}
